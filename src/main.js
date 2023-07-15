85ef0ef945694e43ce707b79ef4e8ff5b9fca086
Pawan Manka <pawanmanka07@gmail.com>
2023-07-15 22:42:26 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// eslint-disable-next-line
+// add this
+// import './css/index.css'
 createApp(App).use(store).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    {{books}}
+    
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +10,35 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      books:[]
+    }
+  },
+  created(){
+    this.getBooks();
+  },
+  methods:{
+    async getBooks(){
+      let res = await axios.post('http://127.0.0.1:8000/graphql', {
+                query: `{
+                  books {
+                    id
+                    title
+                    author
+                  }
+                }`
+              });
+      this.books = res.data.data.books;
+    }
   }
+
 }
 </script>
